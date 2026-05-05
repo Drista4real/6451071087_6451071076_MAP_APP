090afd65ddf39a76db2d825516aea226922a3ad2
ThuanPham <quocthuan1133@gmail.com>
2026-05-05 15:20:36 +0700

    update file

---
+++ lib/data/models/shipping_model.dart
@@ -0,0 +1,40 @@
+class ShippingInfo {
+  String carrier;
+  String trackingNumber;
+  String shippingStatus;
+  String shippingMethod;
+  DateTime? estimatedDelivery;
+  DateTime? deliveredAt;
+
+  ShippingInfo({
+    required this.carrier,
+    required this.trackingNumber,
+    required this.shippingStatus,
+    required this.shippingMethod,
+    this.estimatedDelivery,
+    this.deliveredAt,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'carrier': carrier,
+      'trackingNumber': trackingNumber,
+      'shippingStatus': shippingStatus,
+      'shippingMethod': shippingMethod,
+      'estimatedDelivery': estimatedDelivery,
+      'deliveredAt': deliveredAt,
+    };
+  }
+
+  factory ShippingInfo.fromJson(Map<String, dynamic> json) {
+    return ShippingInfo(
+      carrier: json['carrier'],
+      trackingNumber: json['trackingNumber'],
+      shippingStatus: json['shippingStatus'],
+      shippingMethod: json['shippingMethod'],
+      // Lưu ý: .toDate() thường được dùng khi parse từ Firebase Firestore Timestamp
+      estimatedDelivery: json['estimatedDelivery']?.toDate(),
+      deliveredAt: json['deliveredAt']?.toDate(),
+    );
+  }
+}
